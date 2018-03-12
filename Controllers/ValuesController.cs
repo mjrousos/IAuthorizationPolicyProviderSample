d3f71277b7c3b69a38f1850e2ee783b38bad1c65
Mike Rousos <Mike@MJRousos.com>
2018-03-12 17:39:40 -0400

    Retarget to netcoreapp2.0 since nothing 2.1-specific is needed

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PolicyProvider.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
+    [Controller]
     public class ValuesController : ControllerBase
     {
         // GET api/values
